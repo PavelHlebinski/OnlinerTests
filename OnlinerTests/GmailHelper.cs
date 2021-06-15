ec64fc4faf56dd1ecbfa9562106d9640e71ea044
PavelKhlebinski <predramer@gamil.com>
2021-06-15 17:22:24 +0300

    Add project files.

---
+++ OnlinerTests/GmailHelper.cs
@@ -0,0 +1,66 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace OnlinerTests
+{
+    public class GmailHelper
+    {
+        string[] Scopes = {GmailService.Scope.GmailReadonly};
+        string ApplicationName = "GmailAPI API .NET Quickstart";
+
+        private void GetMail()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
+
+            Console.Read();
+
+            var messageListRequest = service.Users.Messages.List("tseleniumonliner@gmail.com");
+            var messageList = messageListRequest.Execute();
+        }
+    }
+}
+
+
+
